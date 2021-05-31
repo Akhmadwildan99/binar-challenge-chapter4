a21e49254beb682bab934139ec0eed364b4e11fd
Akhmad Wildan <wildanakhmad3584@gmail.com>
2021-05-31 11:48:22 +0700

    berhasil membuat rules dan menemabahkan kelas pada pilihan com

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+// select element
+const pBatu = document.querySelector('.batu');
+const pilihan = document.querySelectorAll('.player li img');
+
+
 // Making Play Rules
 
 function getPilihanCom() {
     const com = Math.floor(Math.random() * 3) + 1;
-    console.log(com);
+    // console.log(com);
     if(com == 1) return 'batu';
     if(com == 2) return 'kertas';
     return 'gunting'; 
@@ -15,22 +20,39 @@ function getHasil(player, com) {
     if(player == 'gunting') return (com == 'batu') ? 'com win':'player 1 win';
 }
 
-console.log(getHasil('kertas', getPilihanCom()))
+// console.log(getHasil('batu', getPilihanCom()));
+
+
+pilihan.forEach((pil) => {
+    pil.addEventListener('click', function() {
+        const pilCom = getPilihanCom();
+        const pilPlayer = pil.className;
+        pil.classList.toggle('active-player');
+        const hasil = getHasil(pilPlayer, pilCom);
+    
+        console.log('p:' + pilPlayer);
+        console.log('c: ' + pilCom);
+    
+        const active = document.querySelectorAll('.com li img');
+        active.forEach((e) => {
+            if(e.className == pilCom) {
+                e.classList.toggle('active-player');
+            }
+    
+        })
+    
+        
+    });
+
+})
 
 
 
 
 
-// function hasil() {
-//     const pilCom = getPilihanCom();
-//     console.log(getPilihanCom());
-//     const pilPlayer = 'batu';
-//     const hasilAkhir = getHasil(pilCom, pilPlayer);
 
-//     return hasilAkhir;
-// }
 
-// console.log(hasil());
+
 
 
 
